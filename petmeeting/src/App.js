8690e17d008f14c022dc8abaa4b1f92a638e9011
Kimsumin123 <tntn211@naver.com>
2023-03-29 18:29:26 +0900

    petmeeting 초기본

---
+++ petmeeting/src/App.js
@@ -0,0 +1,20 @@
+import PetMeetingHeader from "./PetMeetingHeader";
+import PetMeetingFooter from "./PetMeetingFooter";
+import PetMeetingTopBanner from "./PetMeetingTopBanner";
+import PetMeetingBestShow from "./PetMeetingBestShow";
+import PetMeetingCompany from "./PetMeetingCompany";
+
+function App() {
+  return (
+    <div>
+        <PetMeetingHeader/>
+        {/*<PetMeetingTopBanner />*/}
+        {/*<PetMeetingBestShow />*/}
+        {/*<PetMeetingFooter />*/}
+
+
+    </div>
+  );
+}
+
+export default App;
